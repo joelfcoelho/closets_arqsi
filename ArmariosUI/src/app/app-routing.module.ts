d78f46cecaf39d5dbe3ad89aba89200938d6e8d6
1121292@isep.ipp.pt <1121292@isep.ipp.pt>
2018-11-28 22:42:24 +0000

    Bootstrap CSS; Componente 404 e Homepage

--- ArmariosUI/src/app/app-routing.module.ts
+++ ArmariosUI/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+
+import { P404Component } from './pages/404.component';
+
+
+
+const routes: Routes = [
+  { path: 'not-found', component: P404Component },
+  { path: '**', redirectTo: '/not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [P404Component]
 })
 export class AppRoutingModule { }
